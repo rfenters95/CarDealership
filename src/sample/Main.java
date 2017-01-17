ae226f89b05f778bf38647277eab273689cec83c
Reed <rfenters95@gmail.com>
2017-01-17 18:47:37 -0500

    testing uploads

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,6 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        // testing uploads
+        int x = 1;
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
