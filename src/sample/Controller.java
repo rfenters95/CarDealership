93d8b09ccaf6d1ecdcbe20c6167684e422cb3a36
Reed <rfenters95@gmail.com>
2017-01-19 06:22:00 -0500

    added ui

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import util.Customer;
+
 public class Controller {
+
+    @FXML private TextField firstNameTextField;
+    @FXML private TextField lastNameTextField;
+    @FXML private TextField emailTextField;
+    @FXML private TextField phoneTextField;
+    @FXML private TextField addressTextField;
+    @FXML private DatePicker dateOfBirthDatePicker;
+
+    @FXML public void doSomething(ActionEvent event) {
+        Customer customer = new Customer();
+        customer.setFirstName(firstNameTextField.getText());
+        customer.setLastName(lastNameTextField.getText());
+        customer.setEmail(emailTextField.getText());
+        customer.setPhone(phoneTextField.getText());
+        customer.setAddress(addressTextField.getText());
+        customer.setDateOfBirth(dateOfBirthDatePicker.getValue().toString());
+        System.out.println(customer.toString());
+    }
 }
---
+++ src/util/Customer.java
@@ -0,0 +1,63 @@
+package util;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + lastName + dateOfBirth;
+    }
+}
