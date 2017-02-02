a9e595257802c71f28d3589d19a5d4288a80b527
rfenters95 <rfenters95@gmail.com>
2017-02-02 10:15:04 -0500

    updated UI

--- src/sample/CustomerRegistrationController.java
+++ src/sample/CustomerRegistrationController.java
@@ -2,9 +2,16 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import util.Customer;
+
+import java.io.IOException;
 
 public class CustomerRegistrationController {
     @FXML TextField firstName;
@@ -17,5 +24,14 @@ public class CustomerRegistrationController {
     @FXML public void save(ActionEvent event) {
         Customer customer = new Customer(firstName.getText(), lastName.getText());
         System.out.println("Saved new customer: " + customer);
+    }
+
+    @FXML public void cancel(ActionEvent event) throws IOException {
+        Parent register_page = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        Scene register_scene = new Scene(register_page);
+        Stage register_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        register_stage.hide();
+        register_stage.setScene(register_scene);
+        register_stage.show();
     }
 }
--- src/sample/CustomerSearchController.java
+++ src/sample/CustomerSearchController.java
@@ -4,11 +4,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import util.Customer;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -37,6 +43,30 @@ public class CustomerSearchController implements Initializable {
         firstName.setText("");
         lastName.setText("");
         phoneNumber.setText("");
+    }
+
+    @FXML
+    public void modify(ActionEvent event) {
+        firstName.setText("");
+        lastName.setText("");
+        phoneNumber.setText("");
+    }
+
+    @FXML
+    public void remove(ActionEvent event) {
+        firstName.setText("");
+        lastName.setText("");
+        phoneNumber.setText("");
+    }
+
+    @FXML
+    public void exit(ActionEvent event) throws IOException {
+        Parent register_page = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        Scene register_scene = new Scene(register_page);
+        Stage register_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        register_stage.hide();
+        register_stage.setScene(register_scene);
+        register_stage.show();
     }
 
     @Override
