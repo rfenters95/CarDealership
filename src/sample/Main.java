097a4c214bcbc655000cd433dd38dc26f68f2c30
rfenters95 <rfenters95@gmail.com>
2017-02-07 19:32:40 -0500

    test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Car Dealership");
+        //primaryStage.getIcons().add(new Image("sample/icon.png"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setResizable(false);
         ///primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setScene(new Scene(root));
