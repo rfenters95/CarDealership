a034012b512847eea993152ea35bf3114c384428
rfenters95 <rfenters95@gmail.com>
2017-02-25 17:09:10 -0500

    major refactor

--- src/util/Employee.java
+++ src/util/Employee.java
@@ -1,29 +1,63 @@
 package util;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
-public class Employee {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Employee implements Comparable<Employee> {
+
     private String firstName;
     private String lastName;
     private String phone;
     private String email;
     private String address;
+    private String city;
     private String dateOfBirth;
-    private boolean isAdmin;
+    private String jobTitle;
 
-    public Employee(TextField firstName, TextField lastName, TextField phone, TextField email, TextField address, DatePicker dateOfBirth, boolean isAdmin) {
-        this(firstName.getText(), lastName.getText(), phone.getText(), email.getText(), address.getText(), dateOfBirth.getValue().toString(), isAdmin);
+    public Employee(ResultSet resultSet) throws SQLException {
+        this.firstName = resultSet.getString(2).replace(" ", "");
+        this.lastName = resultSet.getString(3).replace(" ", "");
+        this.phone = resultSet.getString(4).replace(" ", "");
+        this.email = resultSet.getString(5).replace(" ", "");
+        this.address = resultSet.getString(6).replace(" ", "");
+        this.city = resultSet.getString(7).replace(" ", "");
+        this.dateOfBirth = resultSet.getDate(8).toString().replace(" ", "");
+        this.jobTitle = resultSet.getString(9).replace(" ", "");
     }
 
-    public Employee(String firstName, String lastName, String phone, String email, String address, String dateOfBirth, boolean isAdmin) {
+    public Employee(TextField firstName, TextField lastName, TextField phone, TextField email, TextField address, TextField city, DatePicker dateOfBirth, ComboBox<String> jobTitle) {
+        this(firstName.getText(), lastName.getText(), phone.getText(), email.getText(), address.getText(), city.getText(), dateOfBirth.getValue().toString(), jobTitle.getSelectionModel().getSelectedItem());
+    }
+
+    public Employee(String firstName, String lastName, String phone, String email, String address, String city, String dateOfBirth, String job) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.dateOfBirth = dateOfBirth;
-        this.isAdmin = isAdmin;
+        this.jobTitle = job;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        int i = lastName.compareTo(o.lastName);
+        if (i == 0) {
+            return firstName.compareTo(o.firstName);
+        }
+        return i;
+    }
+
+    public String getRow() {
+        return firstName + " " + lastName;
+    }
+
+    public boolean isAdmin() {
+        return jobTitle.equals("Admin".replace(" ", ""));
     }
 
     public String getFirstName() {
