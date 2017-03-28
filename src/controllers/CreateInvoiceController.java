c6920894700095065f055647854fbacc7785a5b8
rfenters95 <rfenters95@gmail.com>
2017-03-28 15:14:07 -0400

    updated createInvoice

--- src/controllers/CreateInvoiceController.java
+++ src/controllers/CreateInvoiceController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -50,6 +51,8 @@ public class CreateInvoiceController implements Initializable {
 
     @FXML public void saveInvoice(ActionEvent event) {
 
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
         try {
 
             Invoice invoice = new Invoice(
@@ -71,6 +74,9 @@ public class CreateInvoiceController implements Initializable {
             // Update listView in searchVehicleTab
             Session.alphaController.getSearchVehicleTabController().updateResultSet();
             Session.alphaController.getSearchVehicleTabController().displayResultSet();
+
+            Session.alert("Invoice created!");
+            stage.close();
 
         } catch (Exception e) {
             e.printStackTrace();
